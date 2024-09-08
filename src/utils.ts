b35732fc1b1f02abac6e0ef202967af25bdd8bfd
δυς <d+gh@dhappy.org>
2024-09-07 23:59:59 -0400

    addin’ build date

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,10 @@
-export const ipfs2HTTP = (ipfs: string) => {
-  return ipfs.replace('ipfs://', 'https://ipfs.io/ipfs/');
-}
+export const ipfs2HTTP = (ipfs: string) => (
+  ipfs && ipfs.replace(/^ipfs:\/\//, 'https://ipfs.io/ipfs/') || null
+)
+
+export const isoDate = (date = new Date()) => (
+  date.toLocaleDateString('sv').replace(/-/g, '⁄')
+)
 
 export type Maybe<T> = T | null
+
